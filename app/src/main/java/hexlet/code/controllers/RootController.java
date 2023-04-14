73bc08a66f53435a698cf36131e196de6e614b1c
sergeygurevich <spakro1802@gmail.com>
2023-04-14 22:49:46 +1000

    add URL model

---
+++ app/src/main/java/hexlet/code/controllers/RootController.java
@@ -0,0 +1,11 @@
+package hexlet.code.controllers;
+
+import io.javalin.http.Handler;
+
+public final class RootController {
+
+    public static Handler welcome = ctx -> {
+        ctx.result("Hello World");
+    };
+}
+
---
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -0,0 +1,37 @@
+package hexlet.code.model;
+
+import io.ebean.Model;
+import io.ebean.annotation.WhenCreated;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.Instant;
+
+@Entity
+public class Url extends Model {
+    @Id
+    private long id;
+    private String name;
+    @WhenCreated
+    private Instant createdAt;
+
+    public Url() {
+    }
+    public Url(String name) {
+        this.id = id;
+        this.name = name;
+        this.createdAt = createdAt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+}
