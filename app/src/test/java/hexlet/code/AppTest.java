8ba0609b5cc6bb661d511423690ce4f419e040e8
sergeygurevich <spakro1802@gmail.com>
2023-04-20 23:16:55 +1000

    add test

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,35 +1,127 @@
 package hexlet.code;
 
-import io.javalin.Javalin;
+import hexlet.code.model.Url;
+
+import hexlet.code.model.query.QUrl;
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
+import io.javalin.Javalin;
+import io.ebean.DB;
+import io.ebean.Database;
+import io.ebean.Transaction;
+
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
+    @Test
+    void testInit() {
+        assertThat(true).isEqualTo(true);
+    }
+
     private static Javalin app;
     private static String baseUrl;
-    private static final int SUCCESS_STATUS_RESPONSE = 200;
+    private static Url existingUrl;
+    private static Database database;
+    private static Transaction transaction;
 
     // BEGIN
     @BeforeAll
     public static void beforeAll() {
         app = App.getApp();
-        app.start();
+        app.start(0);
         int port = app.port();
         baseUrl = "http://localhost:" + port;
+        database = DB.getDefault();
+        existingUrl = new Url("https://github.com");
+        existingUrl.save();
     }
+
     @AfterAll
     public static void afterAll() {
         app.stop();
     }
-    @Test void testConnection() {
-        HttpResponse<String> response = Unirest
-                .get(baseUrl + "/")
-                .asString();
-        assertThat(response.getStatus()).isEqualTo(SUCCESS_STATUS_RESPONSE);
+
+    @BeforeEach
+    final void beforeEach() {
+        transaction = DB.beginTransaction();
+    }
+
+    @AfterEach
+    final void afterEach() {
+        transaction.rollback();
+    }
+    @Nested
+    class RootTest {
+        @Test
+        void testIndex() {
+            HttpResponse<String> response = Unirest.get(baseUrl).asString();
+            assertThat(response.getStatus()).isEqualTo(200);
+            assertThat(response.getBody()).contains("Анализатор страниц");
+        }
+    }
+    @Nested
+    class UrlTest {
+        @Test
+        void testIndex() {
+            HttpResponse<String> response = Unirest
+                    .get(baseUrl + "/urls")
+                    .asString();
+            String body = response.getBody();
+
+            assertThat(response.getStatus()).isEqualTo(200);
+            assertThat(body).contains(existingUrl.getName());
+        }
+        @Test
+        void testShow() {
+            HttpResponse<String> response = Unirest
+                    .get(baseUrl + "/urls/1")
+                    .asString();
+            String body = response.getBody();
+
+            assertThat(response.getStatus()).isEqualTo(200);
+            assertThat(body).contains(existingUrl.getName());
+        }
+        @Test
+        void testIncorrectShowId() {
+            HttpResponse<String> response = Unirest
+                    .get(baseUrl + "/urls/100")
+                    .asString();
+            assertThat(response.getStatus()).isEqualTo(404);
+        }
+        @Test
+        void testCreate() {
+            String inputUrl = "https://leetcode.com";
+            HttpResponse<String> responsePost = Unirest
+                    .post(baseUrl + "/urls")
+                    .field("url", inputUrl)
+                    .asEmpty();
+
+            assertThat(responsePost.getStatus()).isEqualTo(302);
+            assertThat(responsePost.getHeaders().getFirst("Location")).isEqualTo("/urls");
+
+            HttpResponse<String> response = Unirest
+                    .get(baseUrl + "/urls")
+                    .asString();
+            String body = response.getBody();
+
+            assertThat(response.getStatus()).isEqualTo(200);
+            assertThat(body).contains(inputUrl);
+            assertThat(body).contains(existingUrl.getName());
+            assertThat(body).contains("Страница успешно добавлена");
+
+            Url actuallUrl = new QUrl()
+                    .name.equalTo(inputUrl)
+                    .findOne();
+
+            assertThat(actuallUrl).isNotNull();
+            assertThat(actuallUrl.getName()).isEqualTo(inputUrl);
+        }
     }
 }
