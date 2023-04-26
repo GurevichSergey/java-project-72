2ad7bac1e7e017293f87936a2e161c9b438048a1
sergeygurevich <spakro1802@gmail.com>
2023-04-27 02:48:20 +1000

    add UrlCheck and refactoring

--- app/src/main/java/hexlet/code/controllers/UrlController.java
+++ app/src/main/java/hexlet/code/controllers/UrlController.java
@@ -1,14 +1,23 @@
 package hexlet.code.controllers;
 
+import hexlet.code.model.UrlCheck;
+import hexlet.code.model.query.QUrlCheck;
 import io.javalin.http.Handler;
 import hexlet.code.model.Url;
 import hexlet.code.model.query.QUrl;
 import io.javalin.http.NotFoundResponse;
-import org.apache.commons.validator.routines.UrlValidator;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import kong.unirest.UnirestException;
 import io.ebean.PagedList;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.parser.Parser;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -39,31 +48,29 @@ public final class UrlController {
         ctx.render("urls/index.html");
     };
     public static Handler createUrl = ctx ->  {
-        String urlString = ctx.formParam("url");
-        UrlValidator urlValidator = new UrlValidator();
-        if (!urlValidator.isValid(urlString)) {
+        try {
+            String urlString = ctx.formParam("url");
+            URL parsedUrl = new URL(urlString);
+            var normalizeUrl = parsedUrl.getProtocol() + "://" + parsedUrl.getAuthority();
+            Url mainUrl = new Url(normalizeUrl);
+            boolean urlContain = new QUrl()
+                    .name.equalTo(normalizeUrl)
+                    .exists();
+            if (urlContain) {
+                ctx.sessionAttribute("flash", "Страница уже существует");
+                ctx.sessionAttribute("flash-type", "info");
+                ctx.redirect("/urls");
+                return;
+            }
+            mainUrl.save();
+            ctx.sessionAttribute("flash", "Страница успешно добавлена");
+            ctx.sessionAttribute("flash-type", "success");
+            ctx.redirect("/urls");
+        } catch (MalformedURLException e) {
             ctx.sessionAttribute("flash", "Некорректный URL");
             ctx.sessionAttribute("flash-type", "danger");
             ctx.render("index.html");
-            return;
         }
-        assert urlString != null;
-        URL parsedUrl = new URL(urlString);
-        var normalizeUrl = parsedUrl.getProtocol() + "://" + parsedUrl.getAuthority();
-        Url mainUrl = new Url(normalizeUrl);
-        boolean urlContain = new QUrl()
-                .name.equalTo(normalizeUrl)
-                .exists();
-        if (urlContain) {
-            ctx.sessionAttribute("flash", "Страница уже существует");
-            ctx.sessionAttribute("flash-type", "info");
-            ctx.redirect("/urls");
-            return;
-        }
-        mainUrl.save();
-        ctx.sessionAttribute("flash", "Страница успешно добавлена");
-        ctx.sessionAttribute("flash-type", "success");
-        ctx.redirect("/urls");
     };
     public static Handler showUrl = ctx -> {
         long id = ctx.pathParamAsClass("id", Long.class).getOrDefault(null);
@@ -73,7 +80,41 @@ public final class UrlController {
         if (url == null) {
             throw new NotFoundResponse();
         }
+        List<UrlCheck> urlChecks = new QUrlCheck()
+                .url.equalTo(url)
+                .orderBy().id.desc()
+                .findList();
+
+        ctx.attribute("urlChecks", urlChecks);
         ctx.attribute("url", url);
         ctx.render("urls/show.html");
+    };
+    public static Handler urlCheck = ctx -> {
+        long id = ctx.pathParamAsClass("id", Long.class).getOrDefault(null);
+        Url url = new QUrl()
+                .id.equalTo(id)
+                .findOne();
+        try {
+            HttpResponse<String> response = Unirest
+                    .get(url.getName())
+                    .asString();
+            Document body = Parser.parse(response.getBody(), url.getName());
+            var statusCode = response.getStatus();
+            String title = body.title();
+            String h1 = Optional.ofNullable(body.selectFirst("h1"))
+                    .map(Element::text)
+                    .orElse("");
+            String description = Optional.ofNullable(body.selectFirst("meta[name=description][content]"))
+                    .map(x -> x.attr("content"))
+                    .orElse("");
+            UrlCheck urlCheck = new UrlCheck(statusCode, title, h1, description, url);
+            urlCheck.save();
+            ctx.sessionAttribute("flash", "Страница успешно проверена");
+            ctx.sessionAttribute("flash-type", "success");
+        } catch (UnirestException e) {
+            ctx.sessionAttribute("flash", "Некорректный адрес");
+            ctx.sessionAttribute("flash-type", "danger");
+        }
+        ctx.redirect("/urls/" + id);
     };
 }
